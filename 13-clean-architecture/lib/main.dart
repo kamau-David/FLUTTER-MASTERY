9f3c9b562b5b88d123173adc11318b8f09e1615e
kamau-David <dayvieek@gmail.com>
2026-03-07 15:00:00 +0300

    feat(13): wire up feature-first clean architecture app entry point

---
+++ 13-clean-architecture/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/tasks/presentation/tasks_screen.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) => const MaterialApp(home: TasksScreen());
+}
